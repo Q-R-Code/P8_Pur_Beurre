bd9af51eeaf03fa3ab50c1d4f9f041d5aebd94e8
Quentin RUFFIER <qruffier@gmail.com>
2021-03-24 17:47:08 +0100

    detail page OK try substitute algo

--- pur_beurre_project/catalogue/views.py
+++ pur_beurre_project/catalogue/views.py
@@ -1,3 +1,6 @@
+import ast
+
+import requests
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
@@ -27,10 +30,30 @@ def search(request):
 
 def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
+    product_categories = ast.literal_eval(product.categories)
+    url = f"https://fr.openfoodfacts.org/cgi/search.pl?action=process&"
+    for i in range(len(product_categories)):
+        url += f"tagtype_{i}=categories&tag_contains_{i}=contains&tag_{i}={product_categories[i]}&"
+    url += "page_size=100&json=true"
+    req = requests.get(url)
+    data = req.json()
+    substitutes = []
+    for x in range(100):
+        score = data["products"][x].get("nutriscore_grade")
+        if score == str("a"):
+            substitutes.append(data["products"][x])
+    if len(substitutes) == 0:
+        for x in range(100):
+            score = data["products"][x].get("nutriscore_grade")
+            if score == str("b"):
+                substitutes.append(data["products"][x])
+    print(substitutes[0])
     context = {
-        "product": product
+        "product": product,
+        "substitutes": substitutes
     }
     return render(request, 'catalogue/detail.html', context)
+
 
 def legal_notice(request):
     return render(request, 'catalogue/legal-notice.html')
