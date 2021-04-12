88a4f1c8a833329b3fefe1bd15f64949fc431a72
Quentin RUFFIER <qruffier@gmail.com>
2021-04-12 17:15:26 +0200

    Functionnal tests OKAY !

--- pur_beurre_project/catalogue/tests.py
+++ pur_beurre_project/catalogue/tests.py
@@ -1,7 +1,12 @@
+import time
+
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from seleniumlogin import force_login
 
 from .models import Product, Sub_saved
 
@@ -19,7 +24,7 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'catalogue/legal-notice.html')
 
-    # Test my page et my product  et essayer de retrouver le 404 si pas co
+
 
 class TestProducts(TestCase):
 
@@ -84,3 +89,61 @@ class TestProducts(TestCase):
         response = self.client.post(reverse('catalogue:delete', args=[sub_id]))
         new_sub = Sub_saved.objects.count()
         self.assertEquals(new_sub, old_sub - 1)
+
+class Test_Functionnal_App_Catalogue(StaticLiveServerTestCase):
+    """Test search , save form submission"""
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        Product.objects.create(
+            name="Produit1",
+            image_url="https://produit1-image-url.fr",
+            categories=['Boissons', 'Eaux', 'breakfasts'],
+            nutriscore_grade="b",
+            image_nutriments="https://produit1-nutriments.fr",
+            barcode="3274080011111",
+            url="https://produit1-url.fr"
+        ).save()
+        Product.objects.create(
+            name="Produit2",
+            image_url="https://produit2-image-url.fr",
+            categories=['Boissons', 'Eaux', 'breakfasts'],
+            nutriscore_grade="b",
+            image_nutriments="https://produit2-nutriments.fr",
+            barcode="3274080022222",
+            url="https://produit2-url.fr"
+        ).save()
+        User.objects.create(
+            username="user1", email="user1@user1.com", password="azerty"
+        ).save()
+
+        self.user1 = User.objects.get(username="user1")
+        self.product1 = Product.objects.get(name="Produit1")
+        self.product2 = Product.objects.get(name="Produit2")
+
+
+    def test_search_and_save_sub_button(self):
+        force_login(self.user1,self.driver, self.live_server_url)
+        self.driver.get(str(self.live_server_url) + '/search/?query=Produit1')
+        search_button = self.driver.find_element_by_id('search-Produit1')
+        search_button.click()
+
+        redirection_url = self.driver.current_url
+        self.assertEquals(self.live_server_url + f"/{self.product1.id}/", redirection_url)
+
+        time.sleep(2)
+
+        save_button = self.driver.find_element_by_id('save-Produit2')
+        save_button.click()
+
+        time.sleep(2)
+
+        redirection_url = self.driver.current_url
+        self.assertEquals(self.live_server_url + "/", redirection_url)
+
+        time.sleep(2)
+
+        self.driver.get(str(self.live_server_url) + '/mes-produits/')
+
+        time.sleep(3)
+
+        self.driver.quit()
